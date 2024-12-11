34afc3f13224d9b1f8572806f91f1dde2e06506a
拓荒者 <eMuBIN@126.com>
2024-12-11 21:00:39 +0800

    Update promise.php
    [REA] 完整的过程, 增加了我自己用的`命名空间`, 有需要的自己改一下就好.

--- promise.php
+++ promise.php
@@ -1,31 +1,34 @@
 <?php
 /**************************************************
 * author: 拓荒者 <eMuBin@126.com>
+* request: PHP 7.0+
 **************************************************/
 
-namespace promise {
-    use Co;
+namespace helper\promise {
+    use Co, helper, ReflectionClass;
 
-    use ReflectionClass;
+    define('helper_promise_states', ['pending', 'canceled', 'rejected', 'fulfilled']);
 
-    define('promise_states', ['pending', 'canceled', 'rejected', 'fulfilled']);
+    define('helper_promise_fail_states', array_slice(helper_promise_states, 1, 2));
 
-    define('promise_fail_states', array_slice(promise_states, 1, 2));
+    define('helper_promise_finally_states', array_slice(helper_promise_states, 1));
 
     class proto {
-        const pending = promise_states[0];
+        const pending = helper_promise_states[0];
 
-        const canceled = promise_states[1];
+        const canceled = helper_promise_states[1];
 
-        const rejected = promise_states[2];
+        const rejected = helper_promise_states[2];
 
-        const fulfilled = promise_states[3];
+        const fulfilled = helper_promise_states[3];
 
         private $id;
 
-        private $state = promise_states[0];
+        private $state = helper_promise_states[0];
 
         private $result;
+
+        private $waiting = false;
 
         private $suspend = false;
 
@@ -33,8 +36,18 @@ namespace promise {
 
         private $resolves = [];
 
+        private $finallys = [];
+
         function __construct(callable $fn = null) {
-            $fn && Go([$this, '_exec'], $fn);
+            helper_debug < 0 && helper_emergency(__METHOD__.':'.spl_object_id($this));
+
+            // $traces = debug_backtrace(); var_dump(join(':', [$traces[1]['file'], $traces[1]['line'], $traces[2]['function'], $traces[2]['class'], spl_object_id($this)]));
+
+            Go([$this, '_exec'], $fn);
+        }
+
+        function __destruct() {
+            helper_debug < 0 && helper_emergency(__METHOD__.':'.spl_object_id($this));
         }
 
         function __get(string $property) {
@@ -46,15 +59,13 @@ namespace promise {
         }
 
         public function wait() {
-            if ( $this->state === self::pending ) {
-                if ( !$this->id ) { $this->id = Co::getCid(); }
+            $this->waiting = true;
 
-                $this->suspend = true; Co::suspend();
+            Co::exists($this->id) && Co::join([$this->id]);
 
-                $this->_canceled();
-            }
+            $result = $this->result; $this->result = null;
 
-            return $this->result;
+            return $result;
         }
 
         public function then(callable $handler) {
@@ -66,18 +77,26 @@ namespace promise {
         }
 
         public function cancel() {
-            $this->id && Co::cancel($this->id);
+            Co::cancel($this->id);
         }
 
         public function finally(callable $handler) {
-            $this->rejects[] = $handler;
-
-            $this->resolves[] = $handler;
-
-            return $this;
+            $this->finallys[] = $handler; return $this;
         }
 
-        private function _exec(callable $fn) {
+        public function rejected() {
+            return in_array($this->state, helper_promise_fail_states);
+        }
+
+        public function finallyed() {
+            return in_array($this->state, helper_promise_finally_states);
+        }
+
+        private function _exec(callable $fn = null) {
+            $this->id = Co::getCid();
+
+            if ( is_null($fn) ) { return Co::suspend(); }
+
             $rc = new ReflectionClass($this);
 
             $reject = $rc->getMethod('_reject');
@@ -89,8 +108,12 @@ namespace promise {
             $this->_canceled();
         }
 
-        private function _result(string $state, $result = null) {
-            if ( $this->state !== self::pending ) { return ; }
+        protected function _state(string $state) {
+            $this->state = $state;
+        }
+
+        protected function _result(string $state, $result = null) {
+            if ( $this->finallyed() ) { return ; }
 
             $this->state = $state;
 
@@ -99,32 +122,38 @@ namespace promise {
             $this->_handler();
         }
 
-        private function _reject($result = null) {
+        protected function _reject($result = null) {
             $this->_result(self::rejected, $result);
         }
 
-        private function _resolve($result = null) {
+        protected function _resolve($result = null) {
             $this->_result(self::fulfilled, $result);
         }
 
-        private function _handler() {
+        protected function _handler() {
             $handlers = &$this->{$this->state === self::fulfilled ? 'resolves' : 'rejects'};
 
             while ($handler = array_shift($handlers)) { $handler($this->result); }
+
+            while ($handler = array_shift($this->finallys)) { $handler($this->result); }
 
             array_splice($this->rejects, 0);
 
             array_splice($this->resolves, 0);
 
-            $this->id && $this->suspend && Co::exists($this->id) && Co::resume($this->id);
+            array_splice($this->finallys, 0);
+
+            is_yield($this->id) && Co::resume($this->id);
+
+            if ( !$this->waiting ) { $this->result = null; $this->waiting = false; }
 
             $this->suspend = false;
 
             return $this;
         }
 
-        private function _canceled() {
-            if ( $this->state === self::pending && Co::isCanceled() ) {
+        protected function _canceled() {
+            if ( !$this->finallyed() && Co::isCanceled() ) {
                 $this->state = $this->result = self::canceled;
             }
         }
@@ -134,31 +163,13 @@ namespace promise {
      * 解忧器
      * @readme 更自由的调用 promise
      */
-    class resolvers {
-        private $promise;
-
-        private $reflection;
-
-        function __construct() {
-            $this->promise = new proto();
-
-            $this->reflection = new ReflectionClass($this->promise);
-        }
-
-        function __get(string $property) {
-            return $property === 'promise' ? $this->promise : null;
-        }
-
-        public function wait() {
-            return $this->promise->wait();
-        }
-
+    class resolvers extends proto {
         public function reject($result = null) {
-            return $this->reflection->getMethod('_reject')->getClosure($this->promise)($result);
+            return $this->_reject($result);
         }
 
         public function resolve($result = null) {
-            return $this->reflection->getMethod('_resolve')->getClosure($this->promise)($result);
+            return $this->_resolve($result);
         }
     }
 
@@ -175,7 +186,7 @@ namespace promise {
 
         private $capacity = 0;
 
-        private $resolvers;
+        private $resolver;
 
         private $finallyed = false;
 
@@ -186,7 +197,7 @@ namespace promise {
 
             $this->capacity = $capacity;
 
-            $this->resolvers = new resolvers();
+            $this->resolver = new resolvers();
 
             $this->dispatch();
         }
@@ -202,14 +213,14 @@ namespace promise {
         }
 
         public function wait() {
-            return $this->resolvers->wait();
+            return $this->resolver->wait();
         }
 
         public function defer() {
             $this->current--;
 
             !$this->finallyed ? Co::resume($this->id) : (
-                $this->current < 1 && $this->resolvers->resolve()
+                $this->current < 1 && $this->resolver->resolve()
             );
         }
 
@@ -221,14 +232,16 @@ namespace promise {
     }
 }
 
-namespace {
-    use promise\{proto, resolvers, restrictor};
+namespace helper {
+    use helper\promise\{proto, resolvers, restrictor};
 
-    class promise extends proto  {
+    class promise extends proto {
         static public function try(callable $fn, ...$args) {
-            return new self(function($resolve, $reject) use ($fn, $args) {
+            return new proto(static function($resolve, $reject) use ($fn, $args) {
                 try {
-                    if ( !(($result = $fn(...$args)) instanceof self) ) { return $resolve($result); }
+                    $result = helper_call_user_func($fn, ...$args);
+
+                    if ( !($result instanceof self) ) { return $resolve($result); }
 
                     $result->wait(); return $result->state === proto::fulfilled ? $resolve($result->result) : $reject($result->result);
                 } catch (Exception $e) {
@@ -238,7 +251,7 @@ namespace {
         }
 
         static public function all(array $promises, bool $settled = false) {
-            return new self(function($resolve, $reject) use (&$promises, $settled) {
+            return new proto(static function($resolve, $reject) use (&$promises, $settled) {
                 $sizeof = sizeof($promises);
 
                 $results = array_fill(0, $sizeof, null);
@@ -258,7 +271,7 @@ namespace {
                             $it->finally(function($result = null) use (&$finallyed, &$results, &$sizeof, $settled, $resolve, $reject, $it, $i) {
                                 if ( $finallyed ) { return ; }
 
-                                if ( !$settled && $it->state === proto::rejected ) {
+                                if ( !$settled && $it->rejected() ) {
                                     $finallyed = true; return $reject($result);
                                 }
 
@@ -274,18 +287,18 @@ namespace {
         static public function any(array $promises) { return self::race($promises); }
 
         static public function race(array $promises) {
-            return new self(function($resolve, $reject) use (&$promises) {
+            return new proto(static function($resolve, $reject) use (&$promises) {
                 $finallyed = false;
 
                 foreach ($promises as $it) {
-                    if ( $it->state !== proto::pending ) {
-                        return in_array($it->state, promise_fail_states, true) ? $reject($it->result) : $resolve($it->result);
+                    if ( $it->finallyed() ) {
+                        return $it->rejected() ? $reject($it->result) : $resolve($it->result);
                     }
 
                     $it->finally(function($result = null) use (&$finallyed, $promises, $resolve, $reject, $it) {
                         if ( $finallyed ?: !($finallyed = true) ) { return ; }
 
-                        in_array($it->state, promise_fail_states, true) ? $reject($result) : $resolve($result);
+                        $it->rejected() ? $reject($result) : $resolve($result);
                     });
                 }
             });
@@ -297,7 +310,7 @@ namespace {
             $result = !is_promise($args[0]) && !is_callable($args[0]) ? array_shift($args) : null;
 
             foreach ($args as $arg) {
-                if ( is_promise($arg) ) { $arg->wait(); }
+                if ( is_promise($arg) ) { $result = $arg->wait(); }
 
                 else if ( is_callable($arg) ) {
                     is_promise($result = $arg($result)) && ($result = $result->wait());
@@ -316,6 +329,13 @@ namespace {
         static public function allsettled(array $promises) { return self::all($promises, true); }
 
         static public function withResolvers() { return self::resolvers(); }
+    }
+}
+
+namespace {
+    function is_yield(int $id) {
+        return is_array($traces = Co::getBackTrace($id)) && in_array(($first = array_shift($traces))['function'], ['yield', 'suspend']) &&
+            $first['class'] === 'Swoole\Coroutine';
     }
 
     function is_promise($any = null): bool {
